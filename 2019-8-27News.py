8f30e015a3b33a470ca75f38f078e98a25ddb5cb
TheQ5 <54635926+TheQ5@users.noreply.github.com>
2019-09-02 23:03:42 +0800

    Add files via upload

--- 2019-8-27News.py
+++ 2019-8-27News.py
@@ -20,7 +20,7 @@ p = requests.get(
 
 p.encoding = "utf8"
 
-
+#取得網址資料
 x = BeautifulSoup(p.text,"html.parser")
 a1 = x.find("div",{
     "id" : "breaknews_body"
@@ -38,12 +38,14 @@ for a3 in a2:
         txt=""
         for dd in b1.find_all("p"):
             txt += dd.text
-        
+
+        #把第一頁資料放進資料表
         cmd.execute(
             "INSERT INTO `udn_news`(`url`,`title`,`content`) VALUES(%s,%s,%s)"
         ,(a4.find("a").attrs["href"],a4.find("a").text,txt))
         conn.commit()
 
+        #把第一頁關鍵字放進資料表
         new_id = cmd.lastrowid
         k = jieba.analyse.extract_tags(txt)
         for kk in k:
@@ -51,30 +53,66 @@ for a3 in a2:
                 "INSERT INTO `udn_keyword`(`news_id`,`keyword`) VALUES(%s,%s)"
             ,(new_id,kk))
             conn.commit()
-times = 0
 
-while times < 10
+#顯示進度
+print("第", 1 ,"頁搜尋完畢")
+
+#第2~10頁資料
+times = 0
+while times < 9:
+
     p = requests.get(
-        "https://udn.com/news/get_breaks_article/" + str(times+2) + "/1/",
+        "https://udn.com/news/get_breaks_article/"+ str(times+2) +"/1/",
     )
 
     p.encoding = "utf8"
 
+    #取得網頁資料
     x = BeautifulSoup(p.text,"html.parser")
     a1 = x.find_all("dt",{
         "class" : "lazyload"
     })
     for i in a1:
-        a2=i.find("a").attrs["href"]
-        print(a2)
-            
-    times += 1
+        r = requests.get(
+            "https://udn.com"+i.find("a").attrs["href"],
+        )
+        r.encoding = "utf8"
 
+
+        e=BeautifulSoup(r.text,"html.parser")
+        title = e.find("h1").text   
+        b1=e.find("div",{"id":"story_body_content"})
+        txt = ""
+        for dd in b1.find_all("p"):
+            txt += dd.text
+
+        #把2~10頁新聞放進資料夾
+        cmd.execute(
+            "INSERT INTO `udn_news`(`url`,`title`,`content`) VALUES(%s,%s,%s)"
+        ,(i.find("a").attrs["href"],title,txt))
+        conn.commit()
+
+        #把2~10頁關鍵字放進資料夾
+        new_id = cmd.lastrowid
+        k = jieba.analyse.extract_tags(txt)
+        for kk in k:
+            cmd.execute(
+                "INSERT INTO `udn_keyword`(`news_id`,`keyword`) VALUES(%s,%s)"
+            ,(new_id,kk))
+            conn.commit()
+
+    #顯示進度
+    print("第",times+2,"頁搜尋完畢")
+    times += 1
+    
+#透過關鍵字找新聞
 UserKey = input("請輸入你要找的關鍵字:")
 
 cmd.execute("SELECT `title`,`url` FROM `udn_news` WHERE `content` OR `title` LIKE CONCAT('%%', %s, '%%')",(UserKey))
 conn.commit()
 x=cmd.fetchall()
+
+#將結果製作成表格
 table = prettytable.PrettyTable(["標題","url"],encoding = "utf8")
 for i in x:
     table.add_row(i)
